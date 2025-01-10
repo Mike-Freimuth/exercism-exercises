9dd013463138b73ae073824fe1fd1988df624459
Michael Freimuth <mikefreimuth5@gmail.com>
2025-01-10 13:25:37 -0800

    finished triangle

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,10 +1,36 @@
 def equilateral(sides):
-    pass
+    
+    return sides[0] > 0 and sides[1] == sides[0] and sides[2] == sides[0]
 
 
 def isosceles(sides):
-    pass
+    
+    if sides[0] <= 0 or sides[1] <= 0 or sides[2] <= 0:
+        return False
+    
+    side_comparisons = [sides[0] + sides[1] <= sides[2], sides[0] + sides[2] <= sides[1], sides[1] + sides[2] <= sides[0]]
+    
+    for x in side_comparisons:
+        if x:  return False
+        
+    
+    if len(set(x for x in sides)) == 3:
+        return False
+    
+    return True
 
 
 def scalene(sides):
-    pass
+        
+    if sides[0] <= 0 or sides[1] <= 0 or sides[2] <= 0:
+        return False
+    
+    side_comparisons = [sides[0] + sides[1] <= sides[2], sides[0] + sides[2] <= sides[1], sides[1] + sides[2] <= sides[0]]
+    
+    for x in side_comparisons:
+        if x:  return False
+    
+    if len(set(x for x in sides)) < 3:
+        return False
+    
+    return True
