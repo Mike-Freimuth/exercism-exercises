3205ca8cb21455561ee611cf89afd40289c1aae1
Michael Freimuth <mikefreimuth5@gmail.com>
2025-01-11 11:26:24 -0800

    finished grains

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,6 +1,11 @@
 def square(number):
-    pass
-
+    
+    if number < 1 or number > 64:
+        raise ValueError("square must be between 1 and 64")
+    
+    return 2 ** (number -1)
+    
 
 def total():
-    pass
+    
+    return sum(square(x) for x in range(1,65))
