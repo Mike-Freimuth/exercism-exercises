a35e197496d02706325474588a62b42a0947c349
Michael Freimuth <mikefreimuth5@gmail.com>
2025-01-11 22:18:03 -0800

    finished bob

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,2 +1,24 @@
 def response(hey_bob):
-    pass
+    
+    characters = set(hey_bob)
+    
+    if len(hey_bob) == 0 or hey_bob.isspace():
+        return 'Fine. Be that way!'
+    
+    question = hey_bob.strip()[-1] == '?'
+        
+    yelling = any(c.isalpha() for c in hey_bob) and hey_bob.upper() == hey_bob
+    '''
+    characters.isdisjoint({'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'})
+    '''
+    
+    if question and yelling:
+        return "Calm down, I know what I'm doing!"
+    
+    if question:
+        return 'Sure.'
+    
+    if yelling:
+        return 'Whoa, chill out!'
+    
+    return 'Whatever.'
