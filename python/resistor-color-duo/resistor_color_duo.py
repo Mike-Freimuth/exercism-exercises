e17cc640e027fbeabb90f87b8f8801ab4a719cef
Michael Freimuth <mikefreimuth5@gmail.com>
2025-01-17 12:21:59 -0800

    finished resistor color duo

--- python/resistor-color-duo/resistor_color_duo.py
+++ python/resistor-color-duo/resistor_color_duo.py
@@ -1,2 +1,20 @@
 def value(colors):
-    pass
+    
+    resistor_values = {
+        'black' : 0,
+        'brown' : 1,
+        'red' : 2,
+        'orange' : 3,
+        'yellow' : 4,
+        'green' : 5,
+        'blue' : 6,
+        'violet' : 7,
+        'grey' : 8,
+        'white' : 9,
+    }
+    
+    output = str(resistor_values[colors[0]]) + str(resistor_values[colors[1]])
+    
+    return int(output)
+    
+    
