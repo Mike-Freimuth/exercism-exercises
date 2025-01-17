3358ec754c4ab214bbf079a409a1cc9feccb1368
Michael Freimuth <mikefreimuth5@gmail.com>
2025-01-16 22:52:00 -0800

    finished resistor color

--- python/resistor-color/resistor_color.py
+++ python/resistor-color/resistor_color.py
@@ -1,6 +1,31 @@
 def color_code(color):
-    pass
+    resistor_values = {
+        'black' : 0,
+        'brown' : 1,
+        'red' : 2,
+        'orange' : 3,
+        'yellow' : 4,
+        'green' : 5,
+        'blue' : 6,
+        'violet' : 7,
+        'grey' : 8,
+        'white' : 9,
+    }
+    
+    return resistor_values[color]
 
 
 def colors():
-    pass
+    
+    return [
+        'black',
+        'brown',
+        'red',
+        'orange',
+        'yellow',
+        'green',
+        'blue',
+        'violet',
+        'grey',
+        'white',
+    ]
