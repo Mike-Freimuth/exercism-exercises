bf5bf10f132500b7df1bb3d8e5f27c9580d3c0d4
Michael Freimuth <mikefreimuth5@gmail.com>
2025-01-16 20:34:36 -0800

    finished rotational cipher

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -1,2 +1,19 @@
 def rotate(text, key):
-    pass
+    
+    letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    message = ''
+    
+    for character in text:
+        
+        if character.isalpha():
+            
+            if character.islower():
+                message += letters[(letters.index(character) + key) % 26]
+                
+            else:
+                message += letters[(letters.index(character.lower()) + key) % 26].upper()
+                
+        else:
+            message += character
+
+    return message
