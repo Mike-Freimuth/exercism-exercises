78e5b42ccb685d4d7c87cefe9af5b5bf488f84c4
Michael Freimuth <mikefreimuth5@gmail.com>
2025-01-17 12:57:10 -0800

    finished resistor color trio

--- python/resistor-color-trio/resistor_color_trio.py
+++ python/resistor-color-trio/resistor_color_trio.py
@@ -1,2 +1,36 @@
 def label(colors):
-    pass
+    
+    resistor_values = {
+        'black' : 0,
+        'brown' : 1,
+        'red' : 2,
+        'orange' : 3,
+        'yellow' : 4,
+        'green' : 5,
+        'blue' : 6,
+        'violet' : 7,
+        'grey' : 8,
+        'white' : 9,
+    }
+    
+    output = str(resistor_values[colors[0]]) + str(resistor_values[colors[1]])
+    
+    for x in range(resistor_values[colors[2]]):
+        output += '0'
+        
+    output = str(int(output))
+    
+    trailing_zeros = resistor_values[colors[2]]
+    if colors[1] == 'black':
+        trailing_zeros += 1
+    
+    if trailing_zeros >= 9:
+        return output[:-9] + ' gigaohms'
+    
+    if trailing_zeros >= 6:
+        return output[:-6] + ' megaohms'
+    
+    if trailing_zeros >= 3:
+        return output[:-3] + ' kiloohms'
+    
+    return output + ' ohms'
