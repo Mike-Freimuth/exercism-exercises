787f92fc371ec0bdd2bbac922a9b4f97518aacbc
Michael Freimuth <mikefreimuth5@gmail.com>
2025-01-21 23:43:41 -0800

    finished secret handshake

--- python/secret-handshake/secret_handshake.py
+++ python/secret-handshake/secret_handshake.py
@@ -1,2 +1,17 @@
 def commands(binary_str):
-    pass
+
+    things = ['jump', 'close your eyes', 'double blink', 'wink']
+    place = 0
+    handshake = []
+
+    for i in binary_str[-4:]:
+        
+        if i == '1':
+            handshake.append(things[place])
+            
+        place += 1    
+
+    if binary_str[-5] == '0':
+        handshake.reverse()
+    
+    return handshake
