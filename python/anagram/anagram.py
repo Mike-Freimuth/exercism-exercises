485d741183fb9fef59b52a186c14763760dbc10e
Michael Freimuth <mikefreimuth5@gmail.com>
2025-01-22 17:55:59 -0800

    finished anagram

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,2 +1,28 @@
 def find_anagrams(word, candidates):
-    pass
+    
+    def make_dictionary(word):
+        
+        letters = {}
+    
+        for letter in word.lower():
+            
+            if letter in letters.keys():
+                letters[letter] += 1
+                
+            else:
+                letters[letter] = 1
+                
+        return letters
+    
+    letters = make_dictionary(word)
+    anagrams = []
+    
+    for candidate in candidates:
+        
+        if candidate.lower() == word.lower():
+            continue
+        
+        if make_dictionary(candidate) == letters:
+            anagrams.append(candidate)
+            
+    return anagrams
