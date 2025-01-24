f4ff8e0b2b5134c90474a4c757fa1583474801ef
Michael Freimuth <mikefreimuth5@gmail.com>
2025-01-24 14:48:23 -0800

    finished flatten

--- python/flatten-array/flatten_array.py
+++ python/flatten-array/flatten_array.py
@@ -1,2 +1,16 @@
 def flatten(iterable):
-    pass
+    
+    flat = []
+    
+    for item in iterable:
+        
+        if item == None:
+            continue
+        
+        elif type(item) == int:
+            flat += [item]
+
+        else:
+            flat += flatten(item)
+            
+    return flat
