a6fd3c396e99b206573466a59fbd4fe2762d5e07
Michael Freimuth <mikefreimuth5@gmail.com>
2025-01-24 14:25:24 -0800

    finished hamming

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,13 @@
 def distance(strand_a, strand_b):
-    pass
+    
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Strands must be of equal length.")
+    
+    errors = 0
+    
+    for i in range(len(strand_a)):
+        
+        if strand_a[i] != strand_b[i]:
+            errors += 1
+            
+    return errors
