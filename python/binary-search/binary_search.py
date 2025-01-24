6c888717303bef4e3afe2f49cc4734013f615d40
Michael Freimuth <mikefreimuth5@gmail.com>
2025-01-24 14:18:11 -0800

    finished binary search

--- python/binary-search/binary_search.py
+++ python/binary-search/binary_search.py
@@ -1,2 +1,28 @@
 def find(search_list, value):
-    pass
+    
+    if len(search_list) == 0 or search_list[-1] < value or search_list[0] > value:
+        raise ValueError("value not in array")
+    
+    lower = 0
+    upper = len(search_list) - 1
+    
+    while upper - lower > 1:
+        
+        middle = (upper + lower) // 2
+        
+        if search_list[middle] == value:
+            return middle
+        
+        if search_list[middle] < value:
+            lower = middle
+            
+        else:
+            upper = middle
+
+    if search_list[upper] == value:
+        return upper
+    
+    if search_list[lower] == value:
+        return lower
+    
+    raise ValueError("value not in array")
