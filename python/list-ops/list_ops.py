8215e3a5b46790ccb8a7169f1479336ed20ba76f
Michael Freimuth <mikefreimuth5@gmail.com>
2025-01-27 21:09:53 -0800

    finished list ops

--- python/list-ops/list_ops.py
+++ python/list-ops/list_ops.py
@@ -1,30 +1,75 @@
 def append(list1, list2):
-    pass
+    
+    return list1 + list2
 
 
 def concat(lists):
-    pass
+    
+    flat = []
+    
+    for item in lists:
+        
+        for i in item:
+            flat.append(i)
+            
+    return flat
 
 
 def filter(function, list):
-    pass
+    
+    filtered = []
+    
+    for item in list:
+        
+        if function(item) == True:
+            filtered.append(item)
+            
+    return filtered
 
 
 def length(list):
-    pass
-
+    
+    counter = 0
+    
+    for item in list:
+        counter += 1
+        
+    return counter
 
 def map(function, list):
-    pass
+    
+    new_list = []
+    
+    for i in range(len(list)):
+        new_list.append(function(list[i]))
 
+    return new_list
 
 def foldl(function, list, initial):
-    pass
+    
+    accumulator = initial
+    
+    for item in list:
+        accumulator = function(accumulator, item)
+        
+    return accumulator
 
 
 def foldr(function, list, initial):
-    pass
+    
+    accumulator = initial
+    
+    for index in range(len(list) - 1, -1, -1):
+        accumulator = function(accumulator, list[index])
+        
+    return accumulator
 
 
 def reverse(list):
-    pass
+    
+    reversed_list = []
+    
+    for item in list:
+        reversed_list.insert(0,item)
+        
+    return reversed_list
