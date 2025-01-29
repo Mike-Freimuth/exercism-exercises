21c326911c516a2bb8273a1cb6a055605bde86da
Michael Freimuth <mikefreimuth5@gmail.com>
2025-01-28 20:08:46 -0800

    finished sum of multiples

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,16 @@
 def sum_of_multiples(limit, multiples):
-    pass
+    
+    set_of_multiples = set()
+    
+    for factor in multiples:
+        
+        if factor == 0:
+            continue
+        
+        multiple = factor
+        
+        while multiple < limit:
+            set_of_multiples.add(multiple)
+            multiple += factor
+            
+    return sum(set_of_multiples)
