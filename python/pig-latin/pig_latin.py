12e45334b940a05d85315dd9ef7ce96b024de325
Michael Freimuth <mikefreimuth5@gmail.com>
2025-02-01 18:25:18 -0800

    finished pig latin

--- python/pig-latin/pig_latin.py
+++ python/pig-latin/pig_latin.py
@@ -1,2 +1,24 @@
 def translate(text):
-    pass
+    
+    vowels = ['a','e','i','o','u','A','E','I','O','U']
+    pl_text = []
+    
+    for word in text.split():
+
+        if word[0] in vowels or word[:2].lower() in ['xr', 'yt']:
+            pl_text.append(word + 'ay')
+        
+        else:
+            beginning = word[0]
+            character = 1
+            while word[character] not in vowels + ['y','Y']:
+                beginning += word[character]
+                character += 1
+                
+            if word[character] == 'u' and beginning[-1] in ['q','Q']:
+                beginning += 'u'
+                character += 1
+                
+            pl_text.append(word[character:] + beginning + 'ay')
+        
+    return ' '.join(pl_text)
