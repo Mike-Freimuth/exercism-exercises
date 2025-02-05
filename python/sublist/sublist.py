fdaa320dd4e8d1f4736826c1c6cc13a98ac3076f
Michael Freimuth <mikefreimuth5@gmail.com>
2025-02-04 21:14:11 -0800

    finished sublist

--- python/sublist/sublist.py
+++ python/sublist/sublist.py
@@ -12,11 +12,47 @@ You can learn more here: https://en.wikipedia.org/wiki/Enumerated_type
 
 # Possible sublist categories.
 # Change the values as you see fit.
-SUBLIST = None
-SUPERLIST = None
-EQUAL = None
-UNEQUAL = None
+SUBLIST = 1
+SUPERLIST = 2
+EQUAL = 3
+UNEQUAL = 4
 
 
 def sublist(list_one, list_two):
-    pass
+    
+    if list_one == list_two:
+        return EQUAL
+    
+    length_one = len(list_one)
+    length_two = len(list_two)
+    
+    if length_one == 0:
+        return SUBLIST
+    
+    if length_two == 0:
+        return SUPERLIST
+    
+    if length_one == length_two:
+        return UNEQUAL
+    
+    if length_one > length_two:
+        
+        for index in range(length_one - length_two + 1):
+            
+            if list_one[index] == list_two[0]:
+                
+                if list_one[index : index + length_two] == list_two:
+                    return SUPERLIST
+        
+        return UNEQUAL
+    
+    else:
+        
+        for index in range(length_two - length_one + 1):
+            
+            if list_two[index] == list_one[0]:
+                
+                if list_two[index : index + length_one] == list_one:
+                    return SUBLIST
+        
+        return UNEQUAL
