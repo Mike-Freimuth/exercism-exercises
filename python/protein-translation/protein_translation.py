350e63a2575107dd47e3ef2ddd0327aa0aa6bdd1
Michael Freimuth <mikefreimuth5@gmail.com>
2025-02-08 20:17:06 -0800

    finished protein translation

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -1,2 +1,33 @@
 def proteins(strand):
-    pass
+    
+    encoding = {
+        'AUG' : 'Methionine',
+        'UUU' : 'Phenylalanine',
+        'UUC' : 'Phenylalanine',
+        'UUA' : 'Leucine',
+        'UUG' : 'Leucine',
+        'UCU' : 'Serine',
+        'UCC' : 'Serine',
+        'UCA' : 'Serine',
+        'UCG' : 'Serine',
+        'UAU' : 'Tyrosine',
+        'UAC' : 'Tyrosine',
+        'UGU' : 'Cysteine',
+        'UGC' : 'Cysteine',
+        'UGG' : 'Tryptophan',
+        'UAA' : 'STOP',
+        'UAG' : 'STOP',
+        'UGA' : 'STOP'
+    }
+
+    proteins = []
+    
+    while len(strand) > 2:
+        
+        if encoding[strand[:3]] == 'STOP':
+            break
+        
+        proteins.append(encoding[strand[:3]])
+        strand = strand[3:]
+        
+    return proteins
