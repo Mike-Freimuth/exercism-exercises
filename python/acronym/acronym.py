fda5a5386d8b80b40a7f80799c2ae47d3e3f16f2
Michael Freimuth <mikefreimuth5@gmail.com>
2025-02-10 19:29:53 -0800

    finished acronym

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,2 +1,29 @@
 def abbreviate(words):
-    pass
+    
+    acronym = []
+    
+    for word in words.split():
+        
+        index = 0
+        length_of_word = len(word)
+        not_a_word = False
+        
+        while word[index].isalpha() == False:
+            index+=1
+            if index > length_of_word - 1:
+                not_a_word = True
+                break
+            
+        if not_a_word:
+            continue
+        
+        acronym.append(word[index].upper())
+        
+        if '-' in word:
+            
+            more_words = word.split('-')[1:]
+            
+            for word in more_words:
+                acronym.append(word[0].upper())
+
+    return ''.join(acronym)
