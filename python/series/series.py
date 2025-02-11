ae6c00cdcfed1f7d133d378db1b3e615a1fde357
Michael Freimuth <mikefreimuth5@gmail.com>
2025-02-11 12:23:06 -0800

    finished series

--- python/series/series.py
+++ python/series/series.py
@@ -1,2 +1,21 @@
 def slices(series, length):
-    pass
+    
+    if length == 0:
+        raise ValueError("slice length cannot be zero")
+    
+    if length < 0:
+        raise ValueError("slice length cannot be negative")
+    
+    if series == None or series == '':
+        raise ValueError("series cannot be empty")
+    
+    if length > len(series):
+        raise ValueError("slice length cannot be greater than series length")
+    
+    output = []
+    
+    for i in range(len(series) - length + 1):
+        output.append(series[i:i+length])
+        
+    return output
+        
