4bcbcd873bc209c8411b906fd8263a538bfebbaa
Michael Freimuth <mikefreimuth5@gmail.com>
2025-02-13 20:32:08 -0800

    finished nth prime

--- python/nth-prime/nth_prime.py
+++ python/nth-prime/nth_prime.py
@@ -1,2 +1,24 @@
 def prime(number):
-    pass
+    
+    if number < 1:
+        raise ValueError('there is no zeroth prime')
+    
+    primes = []
+    candidate = 2
+    
+    while len(primes) < number:
+        
+        prime = True
+        
+        for x in primes:
+            
+            if candidate % x == 0:
+                prime = False
+                break
+
+        if prime:
+            primes.append(candidate)
+            
+        candidate += 1
+        
+    return primes[-1]
