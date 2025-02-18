1350da5446a42ef59e8736e5c1e46a344662c32d
Michael Freimuth <mikefreimuth5@gmail.com>
2025-02-17 18:39:39 -0800

    finished word count

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,2 +1,22 @@
+import string
+
 def count_words(sentence):
-    pass
+    
+    punctuation = string.punctuation.replace("'", "")
+    map = str.maketrans(punctuation, ' ' * len(punctuation))
+    words = sentence.translate(map).lower().split()
+    word_count = {}
+    
+    for word in words:
+        
+        word = word.strip("'")
+        if len(word) == 0:
+            continue
+        
+        if word in word_count.keys():
+            word_count[word] += 1
+            
+        else:
+            word_count[word] = 1
+            
+    return word_count
