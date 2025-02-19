30f207eaa2110f5b148d766ce6537dac5a539f99
Michael Freimuth <mikefreimuth5@gmail.com>
2025-02-18 19:59:10 -0800

    finished proverb

--- python/proverb/proverb.py
+++ python/proverb/proverb.py
@@ -1,2 +1,21 @@
-def proverb():
-    pass
+def proverb(*args, qualifier=None):
+    
+    things = [arg for arg in args]
+    output = []
+    
+    if len(things) == 0:
+        return output
+    
+    for i in range(len(things) - 1):
+        
+        output.append(f'For want of a {things[i]} the {things[i+1]} was lost.')
+        
+    if qualifier == None:
+        qualifier = ''
+        
+    else:
+        qualifier += ' '
+        
+    output.append(f'And all for the want of a {qualifier}{things[0]}.')
+
+    return output
