b41fea98d9e5fc6c3359223391e2158c7bc4590b
Michael Freimuth <mikefreimuth5@gmail.com>
2025-02-19 21:51:24 -0800

    finished luhn

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -1,6 +1,43 @@
 class Luhn:
     def __init__(self, card_num):
-        pass
+        
+        self.card_num = card_num
 
     def valid(self):
-        pass
+        
+        number = self.card_num.replace(' ', '')
+        n = len(number)
+        if n < 2:
+            return False
+        
+        if number.isdigit() == False:
+            return False
+        
+        def replace(number):
+            
+            if 2*number > 9:
+                return 2*number - 9
+            
+            else:
+                return 2*number
+            
+        validation_number = ''
+        
+        for i in range(n):
+            
+            if i % 2 == 0:
+                validation_number = number[n-1-i] + validation_number
+                
+            else:
+                validation_number = str(replace(int(number[n-1-i]))) + validation_number
+                
+        sum = 0
+        
+        for character in validation_number:
+            sum += int(character)
+        
+        if sum % 10 == 0:
+            return True
+        
+        return False
+            
