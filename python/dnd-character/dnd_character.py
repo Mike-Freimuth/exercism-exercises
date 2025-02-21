412346a29e6d5f510e6b2bfa3347069479c6723c
Michael Freimuth <mikefreimuth5@gmail.com>
2025-02-20 22:14:57 -0800

    finished dnd character

--- python/dnd-character/dnd_character.py
+++ python/dnd-character/dnd_character.py
@@ -1,6 +1,28 @@
+
+import random
 class Character:
+    
     def __init__(self):
-        pass
+        
+        self.strength = self.ability()
+        self.dexterity = self.ability()
+        self.constitution = self.ability()
+        self.intelligence = self.ability()
+        self.wisdom = self.ability()
+        self.charisma = self.ability()
+        self.hitpoints = 10 + modifier(self.constitution)
+        
+    def ability(self):
+    
+            rolls = [random.randint(1,6) for i in range(4)]
+            rolls.remove(min(rolls))
+            return sum(rolls)
 
 def modifier(value):
-    pass
+    
+    if (value - 10) % 2 == 0:
+        return (value -10) / 2
+    
+    else:
+        return (value -11) / 2
+
