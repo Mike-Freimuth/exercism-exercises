3d02b92f422176d70f53c179ccc0f61b1bffe673
Michael Freimuth <mikefreimuth5@gmail.com>
2025-02-26 16:45:14 -0800

    finished transpose

--- python/transpose/transpose.py
+++ python/transpose/transpose.py
@@ -1,2 +1,20 @@
 def transpose(text):
-    pass
+    
+    rows = text.split('\n')
+    max_length = max([len(row) for row in rows])
+
+    for i in range(len(rows)):
+        
+        rows[i] = rows[i] + '@'*(max_length - len(rows[i]))
+        
+    transpose = [character for character in rows[0]]
+    
+    for i in range(1,len(rows)):
+        for j in range(max_length):
+            
+            transpose[j] += rows[i][j]
+            
+    for i in range(len(transpose)):
+        transpose[i] = transpose[i].rstrip('@').replace('@',' ')
+            
+    return '\n'.join(transpose)
