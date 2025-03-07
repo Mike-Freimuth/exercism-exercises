9b6fd8210108d880d1444211ec32b8427580a2d0
Michael Freimuth <mikefreimuth5@gmail.com>
2025-03-06 23:20:29 -0800

    finished eliuds eggs

--- python/eliuds-eggs/eliuds_eggs.py
+++ python/eliuds-eggs/eliuds_eggs.py
@@ -1,2 +1,27 @@
+import math
+
 def egg_count(display_value):
-    pass
+    
+    if display_value < 2:
+        return display_value
+    
+    number = display_value
+    binary = ''
+    
+    for i in range(int(math.log2(display_value)), -1, -1):
+        
+        if number >= 2**i:
+            binary += '1'
+            number -= 2**i
+            
+        else:
+            binary += '0'
+            
+    count = 0
+            
+    for digit in binary:
+        
+        if digit == '1':
+            count += 1
+            
+    return count
