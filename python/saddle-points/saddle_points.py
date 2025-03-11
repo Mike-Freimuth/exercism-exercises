1fa92d1041351216753b47fda9cd78ff0c00599a
Michael Freimuth <mikefreimuth5@gmail.com>
2025-03-11 12:18:37 -0700

    finished saddle points

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -1,2 +1,49 @@
 def saddle_points(matrix):
-    pass
+    
+    if len(matrix) == 0:
+        return []
+    
+    columns = len(matrix[0])
+    
+    for row in matrix:
+        if len(row) != columns:
+            raise ValueError("irregular matrix")
+        
+    row_max = []
+    
+    for row in matrix:
+        
+        max = 0
+        max_index = []
+        
+        for i in range(columns):
+            
+            if row[i] > max:
+                max = row[i]
+                max_index = [i]
+                
+            elif row[i] == max:
+                max_index.append(i)
+                
+        row_max.append(max_index)
+            
+    potential_houses = []
+    
+    for i in range(len(row_max)):
+        
+        if len(row_max[i]) > 0:
+            
+            for j in row_max[i]:
+            
+                candidate_height = matrix[i][j]
+                column_min = True
+                for row in matrix:
+                    
+                    if row != matrix[i] and row[j] < candidate_height:
+                        column_min = False
+                        break
+                    
+                if column_min == True:
+                    potential_houses.append({'row' : i + 1, 'column' : j + 1})
+        
+    return potential_houses
