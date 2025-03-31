951e1a2e0c854c29e4c984ae55fad0ca6e8bf327
Michael Freimuth <mikefreimuth5@gmail.com>
2025-03-31 11:33:11 -0700

    finished seive

--- python/sieve/sieve.py
+++ python/sieve/sieve.py
@@ -1,2 +1,24 @@
 def primes(limit):
-    pass
+    
+    numbers = {}
+    
+    for number in range(2, limit + 1):
+        numbers[number] = 'prime'
+        
+    for i in range(2, limit +1):
+        
+        if numbers[i] == 'prime':
+            
+            multiple = 2 * i
+            while multiple <= limit:
+                
+                numbers[multiple] = 'not prime'
+                multiple += i
+                
+    primes = []       
+    
+    for number in numbers.keys():
+        if numbers[number] == 'prime':
+            primes.append(number)
+            
+    return primes
